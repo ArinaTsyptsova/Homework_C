90d09482d15bbcc8a1c7ff8334fd2b0b3c41d9bb
Arina <arinacypcova@gmail.com>
2023-07-04 20:03:37 +0700

    Task_2 corrected and works

---
+++ Task_2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число a: ");
+int numbera = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число b: ");
+int numberb = int.Parse(Console.ReadLine());
+if (numbera > numberb) {
+    Console.WriteLine($" max = {numbera}, min = {numberb}");
+}
+else if (numbera == numberb) {
+    Console.WriteLine("Числа a и b равны");
+}
+else {
+    Console.WriteLine($" min = {numbera}, max = {numberb}");
+}
